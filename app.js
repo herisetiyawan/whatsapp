8848220a30d79232284c96b529fa37395426963f
Heri Setiyawan <heriaileen@gmail.com>
2024-04-16 00:19:56 +0700

    Update app.js
    update add 
    webVersionCache: {
        type: "remote",
        remotePath:
          "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
      },

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ const client = new Client({
     ],
     executablePath: "/usr/bin/chromium",
   },
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
 
 // global.client = client;
