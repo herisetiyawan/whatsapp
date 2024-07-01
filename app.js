79f34baeffffcfd7de9212f5e5f6c3356bae9d6e
Heri Setiyawan <heriaileen@gmail.com>
2024-07-01 23:44:03 +0700

    Update app.js
    update

--- app.js
+++ app.js
@@ -31,17 +31,8 @@ const {
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    //headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      //     "--disable-dev-shm-usage",
-      //     "--disable-accelerated-2d-canvas",
-      //     "--no-first-run",
-      //     "--no-zygote",
-      //     "--single-process", // <- this one doesn't works in Windows
-      //     "--disable-gpu",
-    ],
+    headless: true,
+            args: [ '--no-sandbox', '--disable-gpu', ],
     executablePath: "/usr/bin/chromium",
   },
   webVersionCache: {
