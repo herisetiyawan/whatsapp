bd42c2c9dc3607f87989d5a3ba8d41a9f082bfa9
Heri Setiyawan <heriaileen@gmail.com>
2024-07-04 05:57:56 +0700

    Update app.js
    update client

--- app.js
+++ app.js
@@ -28,18 +28,17 @@ const {
   LocalAuth,
 } = require("./index");
 
+const wwebVersion = '2.2407.3';
+
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-    headless: true,
-            args: [ '--no-sandbox', '--disable-gpu', ],
-    executablePath: "/usr/bin/chromium",
-  },
-  webVersionCache: {
-    type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-  },
+    authStrategy: new LocalAuth(), // your authstrategy here
+    puppeteer: {
+      headless: true , args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    },
 });
 
 // global.client = client;
